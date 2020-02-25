6458995ed84f4c3c1936cb8fb75df6872110da35
Simeon Manolov <simeon.manolov@sumup.com>
2020-02-26 01:42:52 +0200

    bump (to v1.x.x, already on prod)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixParams.MixProject do
   def project do
     [
       app: :phoenix_params,
-      version: "0.4.2",
+      version: "1.0.0",
       elixir: ">= 1.6.0",
       start_permanent: Mix.env() == :prod,
       description: description(),
